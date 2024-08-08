854b350f8e15a7718816f49c77bdf605995c0cee
Yaguera <ya-gotre@hotmail.com>
2024-08-08 16:17:29 -0300

    feat: add initial sheet ui component setup from shadcn

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,19 +1,29 @@
-import Image from "next/image";
-import { Card, CardContent } from "./ui/card";
-import { Button } from "./ui/button";
-import { MenuIcon } from "lucide-react";
+import Image from "next/image"
+import { Card, CardContent } from "./ui/card"
+import { Button } from "./ui/button"
+import { MenuIcon } from "lucide-react"
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet"
 
 const Header = () => {
-    return ( 
-        <Card>
-            <CardContent className="p-5 flex justify-between flex-row items-center">
-                <Image alt="FSW Barber" src="/Logo.png" height={18} width={120}/>
-                <Button size="icon" variant="outline">
-                    <MenuIcon/>
-                </Button>
-            </CardContent>
-        </Card>
-     );
+  return (
+    <Card>
+      <CardContent className="flex flex-row items-center justify-between p-5">
+        <Image alt="FSW Barber" src="/Logo.png" height={18} width={120} />
+        <Button size="icon" variant="outline">
+          <MenuIcon />
+        </Button>
+
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button size="icon" variant="outline">
+              <MenuIcon />
+            </Button>
+          </SheetTrigger>
+          <SheetContent></SheetContent>
+        </Sheet>
+      </CardContent>
+    </Card>
+  )
 }
- 
-export default Header;
+
+export default Header
