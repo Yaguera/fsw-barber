3b16780c7fe8537c68947ae68a4c80c79cc4f9bf
Yaguera <ya-gotre@hotmail.com>
2024-08-08 15:13:23 -0300

    feat:add ServiceItem component to ensure barbershop's services are visible on barbershop's page

--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -1,3 +1,4 @@
+import ServiceItem from "@/app/_components/service-item"
 import { Button } from "@/app/_components/ui/button"
 import { db } from "@/app/_lib/prisma"
 import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react"
@@ -17,8 +18,13 @@ const BarbershopPage = async ({ params }: BarbershopPageProps) => {
     where: {
       id: params.id,
     },
+    // incluir a coluna de services na consulta
+    include: {
+      services: true,
+    },
   })
 
+  // Mostrar o erro 404 caso coloque um id inexistente na URL
   if (!barbershop) {
     return notFound()
   }
@@ -72,6 +78,15 @@ const BarbershopPage = async ({ params }: BarbershopPageProps) => {
       <div className="space-y-2 border-b border-solid p-5">
         <h2 className="text-xs font-bold uppercase text-gray-400">Sobre nós</h2>
         <p className="text-justify text-sm">{barbershop?.description}</p>
+      </div>
+
+      <div className="p-5">
+        <h2 className="text-xs font-bold uppercase text-gray-400">Serviços</h2>
+        <div className="space-y-3">
+          {barbershop.services.map((service) => (
+            <ServiceItem key={service.id} service={service} />
+          ))}
+        </div>
       </div>
     </div>
   )
